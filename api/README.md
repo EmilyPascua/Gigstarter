e73d252512c80536c7b3cd5d75b70a5571e40d32
lisamso <lisasoem@live.no>
2018-09-14 20:52:40 -0700

    Created a base template for Spring Boot API.

---
+++ api/README.md
@@ -0,0 +1,5 @@
+
+To run API locally use command:
+  mvn spring-boot:run
+
+  The back end will run on localhost:8080
---
+++ api/src/main/Java/gigstarter/api/Controllers/SampleController.java
@@ -0,0 +1,14 @@
+package gigstarter.api.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "This is a controller";
+    }
+
+}
