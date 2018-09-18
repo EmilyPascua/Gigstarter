006053d2c86388c16f0c7bb6d449b0f862f5e8cf
Calhuang <calhuang@ucdavis.edu>
2018-09-17 23:25:53 -0700

    finished the other half of the home page

--- app/src/components/Jumbotron/Jumbotron.js
+++ app/src/components/Jumbotron/Jumbotron.js
@@ -2,14 +2,24 @@ import React from 'react';
 import styles from './Jumbotron.css';
 import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import Container from 'react-bootstrap/lib/Container';
+import Button from 'react-bootstrap/lib/Button';
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
-const Jumbo = () => {
+
+const Jumbo = ({ isVisible }) => {
+
   return (
     <div>
-      <Jumbotron fluid className={styles.Jumbo}>
+      <Jumbotron fluid className={styles.Jumbo} >
         <Container className={styles.Container}>
-          <h1>Welcome to GigStarter</h1>
+          <h1 id="jum">Welcome to GigStarter</h1>
           <p>A brighter future is just 1 click away</p>
+          <p><Button variant="outline-primary" size="lg" >Let's get started!</Button></p>
+          <p><Button variant="outline-secondary" size="lg" onClick={() => scroller.scrollTo("Sec1", {
+            duration: 1000,
+            delay: 50,
+            smooth: "easeInOutQuint",
+          })}>I want to learn more!</Button></p>
         </Container>
       </Jumbotron>
     </div>
--- app/src/components/Sections/Sections.js
+++ app/src/components/Sections/Sections.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Sections.css';
+import Emoji from "../EmojiSupport/Emoji";
 import Container from 'react-bootstrap/lib/Container';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import ListGroup from 'react-bootstrap/lib/ListGroup';
 import Form from 'react-bootstrap/lib/Form';
+import Card from 'react-bootstrap/lib/Card';
 import searchicon from '../../assets/search.svg';
 import envelope from '../../assets/envelope.svg';
 import growth from '../../assets/growth.svg';
@@ -15,7 +17,7 @@ import presentation from '../../assets/presentation.svg';
 const Sections = () => {
   return (
     <div>
-      <Container className={styles.Sec1}>
+      <Container className={styles.Sec1} id="Sec1">
         {/* <img className={styles.bg1} src={bg1} alt="bg1" /> */}
         <Row className={styles.Sec1Row1}>
           <Col className={styles.Bodyone} xs={6}>
@@ -54,6 +56,21 @@ const Sections = () => {
           </Col>
         </Row>
       </Container>
+      <Container className={styles.Sec3}>
+        <Row className={styles.Sec3Row1}>
+          <Col>
+            <h1>Be Part of the Gig Economy</h1>
+            <ListGroup className={styles.Sec3ListG}>
+              <ListGroup.Item variant="primary">Work at your own pace <Emoji symbol="🚴‍" label="bike" /></ListGroup.Item>
+              <ListGroup.Item variant="secondary">Gain real-world experience <Emoji symbol="🌎" label="globe" /></ListGroup.Item>
+              <ListGroup.Item variant="success">Make primary/secondary income <Emoji symbol="💰" label="money" /></ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col>
+            <iframe title="gigeconomy" width="640" height="360" src="https://www.youtube.com/embed/ul2Jieu8Pto" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </Col>
+        </Row>
+      </Container>
       <Container className={styles.Sec2}>
         <Row className={styles.Sec2Row1}>
           <Col className={styles.Sec2Col1}>
@@ -78,21 +95,7 @@ const Sections = () => {
           </Col>
         </Row>
       </Container>
-      <Container className={styles.Sec3}>
-        <Row>
-          <Col>
-            <h1>Be Part of the Gig Economy</h1>
-            <ListGroup variant="flush">
-              <ListGroup.Item>Work at your own pace</ListGroup.Item>
-              <ListGroup.Item>Gain real-world experience</ListGroup.Item>
-              <ListGroup.Item>Make primary/secondary income</ListGroup.Item>
-            </ListGroup>
-          </Col>
-          <Col>
-            <img src="https://via.placeholder.com/720x480" alt="placeholder" />
-          </Col>
-        </Row>
-      </Container>
+
       <Container className={styles.Sec4}>
         <Row>
           <Col>
@@ -115,19 +118,24 @@ const Sections = () => {
           </Col>
         </Row>
       </Container>
-      <Container className={styles.Sec5}>
-        <Row>
-          <Col>
-            <h1>Reach Us</h1>
-            <p>
-              Call us or email to find out more information about Gigstarter
-            </p>
-            <p>5151 State University Dr, Los Angeles, CA 90032</p>
-            <p>1.800.555.555</p>
-            <p>contact@email.com</p>
-          </Col>
-          <Col>
-            <h1>Leave A Message</h1>
+      <div className={styles.Sec5}>
+
+
+        <Card bg="secondary" border="success" className={styles.Sec5Card1}>
+          <Card.Header><h1>Reach Us</h1></Card.Header>
+          <Card.Body>
+            <Card.Title>Call us or email to find out more information about Gigstarter</Card.Title>
+            <Card.Text>
+              <p>5151 State University Dr, Los Angeles, CA 90032</p>
+              <p>1.800.555.555</p>
+              <p>contact@email.com</p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card bg="secondary" border="success" className={styles.Sec5Card2}>
+          <Card.Header> <h1>Leave A Message</h1></Card.Header>
+          <Card.Body>
             <Form>
               <Form.Group controlId="exampleForm.ControlInput1">
                 <Form.Label>Name</Form.Label>
@@ -142,9 +150,12 @@ const Sections = () => {
                 <Form.Control as="textarea" rows="3" />
               </Form.Group>
             </Form>
-          </Col>
-        </Row>
-      </Container>
+          </Card.Body>
+        </Card>
+
+
+
+      </div>
     </div>
   );
 };
