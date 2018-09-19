ae2d9c301526c94adaf739d8c6d73f6f582d33b4
lisamso <lisasoem@live.no>
2018-09-18 17:55:36 -0700

    Added nessecary classes for authentication.

--- api/src/main/Java/gigstarter/api/MainApplicationClass.java
+++ api/src/main/Java/gigstarter/api/MainApplicationClass.java
@@ -1,10 +1,20 @@
 package gigstarter.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class MainApplicationClass {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Application works!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplicationClass.class, args);
