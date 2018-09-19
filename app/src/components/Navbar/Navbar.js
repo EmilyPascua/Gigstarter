f2129df202fa21254ebf45bbdba0d2c16ba64bbe
Calhuang <calhuang@ucdavis.edu>
2018-09-18 23:00:39 -0700

    added routing and set up login page

--- app/src/components/Navbar/Navbar.js
+++ app/src/components/Navbar/Navbar.js
@@ -5,16 +5,16 @@ import Nav from 'react-bootstrap/lib/Nav';
 
 const NavBar = (props) => {
   return (
-    <div>
+    <div className={styles.Nav}>
       <Navbar expand="lg" fixed="top" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Gigstarter</Navbar.Brand>
+        <Navbar.Brand href="home">Gigstarter</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto" />
           <Nav>
-            <Nav.Link href="#home">About Us</Nav.Link>
-            <Nav.Link href="#link">Gigs</Nav.Link>
-            <Nav.Link href="#login">Login</Nav.Link>
+            <Nav.Link href="about">About Us</Nav.Link>
+            <Nav.Link href="gigs">Gigs</Nav.Link>
+            <Nav.Link href="login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
