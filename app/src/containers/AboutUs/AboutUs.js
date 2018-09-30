bf618886e86bfd2bb69089f5c552f54f4a732505
EmilyPascua <epascuacollege@gmail.com>
2018-09-30 13:08:46 -0700

    Temporary Basic Layout for Gigs/Login page

--- app/src/containers/AboutUs/AboutUs.js
+++ app/src/containers/AboutUs/AboutUs.js
@@ -1,13 +1,71 @@
 import React, { Component } from 'react';
 import styles from './AboutUs.css';
 import Navbar from "../../components/Navbar/Navbar";
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import Alex from '../../assets/teamPictures/alex.jpg';
+import Marco from '../../assets/teamPictures/marco.png';
+import Martin from '../../assets/teamPictures/martin.png';
+import Oscar from '../../assets/teamPictures/oscar.png';
+
 
 class About extends Component {
   render() {
     return (
       <div className={styles.About}>
         <Navbar />
-        About
+        <div className={styles.Header}>
+        <Row >
+          <Col>
+            <h1>Our Mission is to<br></br>Jumpstart Student Careers</h1>
+          </Col>
+        </Row>
+        </div>
+
+        <div className={styles.Container}>
+        <Row>
+          <Col>
+            Gigstarter provides an opportunity for students to gain the practical experience<br></br> needed to excel in their field. Our one-of-a-kind search engine allows students <br></br>to filter through hundreds of gig opportunities to find the perfect ones for you.          </Col>
+        </Row>
+        </div>
+        <br></br>
+        <h1>Get Your Gigstarted</h1>
+
+         <div className={styles.Container}>
+          <Row className="show-grid">
+            <Col><h1><i class="fa fa-book"></i></h1>
+            <h3>Step 1</h3>
+            Scroll through hundreds of gig postings offered on Gigstarter</Col>
+            <Col><h1><i class="fa fa-envelope"></i></h1>
+            <h3>Step 2</h3>
+            If you show interest in a gig, contact the employer to discover the details of the job.</Col>
+            <Col><h1><i class="fa fa-dollar"></i></h1>
+            <h3>Step 2</h3>
+            Get paid for the work you have done! Aside from the extra money, you have now expanded your resume in a positive and professional direction.</Col>
+          </Row>
+        </div>
+        <div className={styles.Container}>
+         <Row><Col><h1>Creative Team</h1>
+        We are a group of dedicated, passionate, and driven individuals working<br></br>towards the common goal of providing professional work experience for students
+         </Col></Row>
+         </div>
+        <div className={styles.Container}>
+          <Row>
+            <Col><img src={Martin} alt="martinPotrait" width="200px" />
+            <h3>Martin Tejeda</h3>
+            Scroll through hundreds of gig postings offered on Gigstarter</Col>
+            <Col><img src={Marco} alt="marcoPotrait" width="200px" />
+            <h3>Marco Garza</h3>
+            If you show interest in a gig, contact the employer to discover the details of the job.</Col>
+            <Col><img src={Alex} alt="alexPotrait" width="200px" />
+            <h3>Alex Lam</h3>
+            Get paid for the work you have done! Aside from the extra money, you have now expanded your resume in a positive and professional direction.</Col>
+            <Col><img src={Oscar} alt="oscarPotrait" width="200px" />
+            <h3>Oscar Aguayo</h3>
+            Scroll through hundreds of gig postings offered on Gigstarter</Col>
+          </Row>
+        </div>
       </div>
     );
   }
--- app/src/containers/Gigs/Gigs.js
+++ app/src/containers/Gigs/Gigs.js
@@ -1,14 +1,121 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import styles from './Gigs.css';
 import Navbar from "../../components/Navbar/Navbar";
+import Card from "react-bootstrap/lib/Card";
+import Button from "react-bootstrap/lib/Button";
+
+import Form from "react-bootstrap/lib/Form";
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
 
 class Gigs extends Component {
   render() {
     return (
       <div className={styles.Gigs}>
         <Navbar />
-        Gigs
-      </div>
+       <div className={styles.Container}>
+          <div className={styles.TopBanner}>
+            <Form>
+            <Row className="show-grid">
+            <Col lg={6} md={1}>
+                <Form.Group controlId="formKeyword">
+                  <div align="left">
+                    <h3><b>Find Gig</b></h3>
+                    <Form.Text className="text-muted">
+                      gig name, keywords, or company name
+                    </Form.Text>
+                  </div>
+                  <Form.Control />
+                  </Form.Group>
+            </Col>
+            <Col lg={6} md={1}>
+                <Form.Group controlId="formLocation">
+                <div align="left">
+                  <h3><b>Location</b></h3>
+                  <Form.Text className="text-muted">
+                    city, state, or zip code
+                  </Form.Text>
+                </div>                  
+                <Form.Control type="location" placeholder="Los Angeles, CA" />
+                </Form.Group>
+                <div align="right">
+                  <Button variant="success" type="submit">
+                    Find Gigs
+                  </Button>
+                </div>
+            </Col>
+            
+            </Row>
+            </Form>
+          </div>
+        </div>
+         <div className={styles.Item}>
+            <Row>
+              <Card className={styles.Test}>
+                <div className={styles.GigTitle}>Event Cook</div>
+                <div className={styles.Employer}>Posted by Eric Brady</div>
+                <div className={styles.GigContent}>
+                <i class="fa fa-dollar"></i> 300<br></br>
+                <i class="fa fa-suitcase"></i> One-Day Gig <br></br>
+                <i class="fa fa-map-marker"></i> 2199 Angeles Avenue <br></br>
+                </div>
+              </Card>
+            </Row>
+          </div>
+          <div className={styles.Item}>
+            <Row>
+              <Card className={styles.Test}>
+                <div className={styles.GigTitle}>Web Design</div>
+                <div className={styles.Employer}>Posted by Roger Zhao</div>
+                <div className={styles.GigContent}>
+                <i class="fa fa-dollar"></i> 200<br></br>
+                <i class="fa fa-suitcase"></i> One-Week Gig <br></br>
+                <i class="fa fa-map-marker"></i> 1022 Alhambra Road <br></br>
+                </div>
+              </Card>
+            </Row>
+          </div>
+          <div className={styles.Item}>
+            <Row>
+              <Card className={styles.Test}>
+                <div className={styles.GigTitle}>Community Fundraiser</div>
+                <div className={styles.Employer}>Posted by GetStarted.com</div>
+                <div className={styles.GigContent}>
+                <i class="fa fa-dollar"></i> 50<br></br>
+                <i class="fa fa-suitcase"></i> One-Day Gig <br></br>
+                <i class="fa fa-map-marker"></i> 5022 Random Avenue <br></br>
+                </div>
+              </Card>
+            </Row>
+          </div>
+          <div className={styles.Item}>
+            <Row>
+              <Card className={styles.Test}>
+                <div className={styles.GigTitle}>Event Cook</div>
+                <div className={styles.Employer}>Posted by Eric Brady</div>
+                <div className={styles.GigContent}>
+                <i class="fa fa-dollar"></i> 300<br></br>
+                <i class="fa fa-suitcase"></i> One-Day Gig <br></br>
+                <i class="fa fa-map-marker"></i> 2199 Angeles Avenue <br></br>
+                </div>
+              </Card>
+            </Row>
+          </div>
+          <div className={styles.Item}>
+
+          {/*Test Pagnitation that doesn't work.*/}
+          <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+          </ul>
+        </nav></div>
+          </div>
     );
   }
 }
