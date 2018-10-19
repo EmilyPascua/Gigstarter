41960dd6cc5d4454b112666b202329bba8d3f128
EmilyPascua <epascuacollege@gmail.com>
2018-10-19 07:40:14 -0700

    Added changes onto User Model

--- api/src/main/Java/gigstarter/api/model/StudentUser.java
+++ api/src/main/Java/gigstarter/api/model/StudentUser.java
@@ -9,6 +9,10 @@ public class StudentUser extends ApplicationUser {
 
     private int year;
 
+    private String industry;
+
+    private String school;
+
     public String getMajor() {
         return major;
     }
@@ -23,5 +27,21 @@ public class StudentUser extends ApplicationUser {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public int getindustry() {
+        return industry;
+    }
+
+    public void setindustry(String industry) {
+        this.industry = industry;
+    }
+
+    public int getschool() {
+        return school;
+    }
+
+    public void setschool(String school) {
+        this.year = school;
     }
 }
